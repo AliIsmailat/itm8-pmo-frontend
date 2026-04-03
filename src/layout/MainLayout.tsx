ed579073821f65c3aedafd4f54f62b805c86e810
AliIsmailat <ali.ismailat@gritacademy.se>
2026-04-03 03:12:54 +0200

    added feedback button

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { ReactNode } from "react";
 import Navbar from "../components/ui/Navbar";
+import FeedbackButton from "../components/ui/FeedbackButton";
 
 type MainLayoutProps = {
   children: ReactNode;
@@ -11,6 +12,7 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-1 bg-[#f7f5fb] overflow-x-hidden">{children}</main>
+      <FeedbackButton />
     </div>
   );
 };
