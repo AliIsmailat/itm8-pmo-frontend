b76d4fe557aa45878e5c2bca645dbf3799bbc030
Adrian Zafari <adrza@itm8.com>
2026-04-09 15:53:09 +0000

    Updated main.tsx: removed handleRedirectPromise(). Obsolete in newer React vesions

--- src/main.tsx
+++ src/main.tsx
@@ -7,21 +7,12 @@ import App from "./App";
 
 import "./index.css";
 
-msalInstance.initialize().then(() => {
-  msalInstance
-    .handleRedirectPromise()  // CODER REVIEW: Denna är inte nödvändig att anropa manuellt, den körs automatiskt av MSAL när appen startar. Se https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/FAQ.md#how-do-i-handle-the-redirect-flow-in-a-react-app
-    .then(() => {
-      ReactDOM.createRoot(document.getElementById("root")!).render(
-        <React.StrictMode>
-          <MsalProvider instance={msalInstance}>
-            <BrowserRouter>
-              <App />
-            </BrowserRouter>
-          </MsalProvider>
-        </React.StrictMode>,
-      );
-    })
-    .catch((error) => {
-      console.error("MSAL redirect error:", error);
-    });
-});
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <MsalProvider instance={msalInstance}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MsalProvider>
+  </React.StrictMode>,
+);
